a84fd1cfdea0d9fc5385b9c2037c6ae63b0f1974
Morten Bertz <morten@telethon.jp>
2021-06-21 12:45:58 +0900

    system tokenizer

--- Sources/Mecab-Swift/Dictionary.swift
+++ Sources/Mecab-Swift/Dictionary.swift
@@ -11,19 +11,32 @@ import Foundation
 A wrapper around a dictionary, for example IPADic. A number of dictionaries for mecan can be found on the internet. Different dictionaries privde different features (POS tagging etc), and so far I have not been able to get this information out of mecab at runtime. The dictionary scheme is contained in the dicrc file.
 */
 public struct Dictionary:CustomStringConvertible, TokenIndexProviding, PartOfSpeechProviding{
+    
     /**
      A dictionary type. This type (will eventually) encapsulate the positional information of the output of the dictionary (POS etc). So far, only IPADic is implemented.
      */
     public enum DictionaryType:CustomStringConvertible{
+        #if canImport(CoreFoundation)
+        case systemTokenizer
+        #endif
+        
         case ipadic
         
         public var description: String{
             switch self {
+            case .systemTokenizer:
+                return "System Tokenizer"
             case .ipadic:
                 return "IPADic"
             }
         }
     }
+    
+   
+    #if canImport(CoreFoundation)
+    public static let systemDictionary = Dictionary(url: URL(fileURLWithPath: ""), type: .systemTokenizer)
+    #endif
+    
     
     let url:URL
     let type:DictionaryType
@@ -35,6 +48,10 @@ public struct Dictionary:CustomStringConvertible, TokenIndexProviding, PartOfSpe
     
     public var description: String{
         return "Dictionary: \(url), Type: \(type)"
+    }
+    
+    var readingIndex: Int{
+        return self.type.readingIndex
     }
     
     var pronunciationIndex: Int{
